68e107c7e2c41e567fddff434b4a1d70b5ea1771
João Pedro Vitorino <joaopver10@outlook.com>
2024-06-14 13:58:11 -0300

    Modificação Settings

--- LojaDeGames/settings.py
+++ LojaDeGames/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,14 +80,22 @@ WSGI_APPLICATION = 'LojaDeGames.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://postgres:nTrATydCHXtNjpTMdiLHqoQoZzVUSKjP@roundhouse.proxy.rlwy.net:36503/railway',
+        conn_max_age=600,
+        ssl_require= DEBUG
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
