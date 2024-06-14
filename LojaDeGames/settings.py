c68c362c930f5a26564df8103997c3fc3cc33b62
João Pedro Vitorino <joaopver10@outlook.com>
2024-06-14 14:15:37 -0300

    Modificação Settings

--- LojaDeGames/settings.py
+++ LojaDeGames/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_vnm3rcz@fy@koed^il!2in17_6)mm-)+%r_1ivwi=t-ih*nc7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
